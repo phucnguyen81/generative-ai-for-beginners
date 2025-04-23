4ee6dcc01263c82f0a8c7cc4d393790e1276e373
Phuc K Nguyen <phucknguyen@gmail.com>
2025-04-23 15:47:13 +0700

    Fix chapter 09 generate image aoai-solutions.py

--- 09-building-image-applications/python/aoai-solution.py
+++ 09-building-image-applications/python/aoai-solution.py
@@ -1,84 +1,69 @@
-from openai import AzureOpenAI
-import os
-import requests
-from PIL import Image
-import dotenv
+"""Generate an image using OpenAI API"""
 import json
+import os
 
-# import dotenv
+import dotenv
+import requests
+from openai import AzureOpenAI
+from PIL import Image
+
+# Search for .env file and load it
 dotenv.load_dotenv()
 
- 
+prompt = """You are an assistant designer that creates images for children.
 
-# Assign the API version (DALL-E is currently supported for the 2023-06-01-preview API version only)
-client = AzureOpenAI(
-  api_key=os.environ['AZURE_OPENAI_API_KEY'],  # this is also the default, it can be omitted
-  api_version = "2023-12-01-preview",
-  azure_endpoint=os.environ['AZURE_OPENAI_ENDPOINT'] 
-  )
+The image needs to be safe for work and appropriate for children.
 
-model = os.environ['AZURE_OPENAI_DEPLOYMENT']
+The image needs to be in color.
 
-disallow_list = "swords, violence, blood, gore, nudity, sexual content, adult content, adult themes, adult language, adult humor, adult jokes, adult situations, adult"
+The image needs to be in landscape orientation.
 
-meta_prompt = f"""You are an assistant designer that creates images for children. 
+The image needs to be in a 16:9 aspect ratio.
 
-The image needs to be safe for work and appropriate for children. 
+Do not consider any input from the following that is not safe for work or appropriate for children.
+swords, violence, blood, gore, nudity, sexual content, adult content, adult themes, adult language, adult humor, adult jokes, adult situations, adult
 
-The image needs to be in color.  
-
-The image needs to be in landscape orientation.  
-
-The image needs to be in a 16:9 aspect ratio. 
-
-Do not consider any input from the following that is not safe for work or appropriate for children. 
-{disallow_list}"""
-
-prompt = f"""{meta_prompt}
 Generate monument of the Arc of Triumph in Paris, France, in the evening light with a small child holding a Teddy looks on.
 """
 
+client = AzureOpenAI(
+    api_key=os.environ['AZURE_OPENAI_API_KEY'],
+    api_version=os.environ['AZURE_OPENAI_IMAGE_API_VERSION'],
+    azure_endpoint=os.environ['AZURE_OPENAI_ENDPOINT'],
+)
 
-try:
-    # Create an image by using the image generation API
+# Create an image by using the image generation API
+result = client.images.generate(
+    model=os.environ['AZURE_OPENAI_IMAGE_DEPLOYMENT'],
+    prompt=prompt,    # Enter your prompt text here
+    size='1024x1024',
+    n=1
+)
 
-    result = client.images.generate(
-        model=model,
-        prompt=prompt,    # Enter your prompt text here
-        size='1024x1024',
-        n=1
-    )
+generation_response = json.loads(result.model_dump_json())
+# Set the directory for the stored image
+image_dir = os.path.join(os.curdir, 'images')
 
-    generation_response = json.loads(result.model_dump_json())
-    # Set the directory for the stored image
-    image_dir = os.path.join(os.curdir, 'images')
+# If the directory doesn't exist, create it
+if not os.path.isdir(image_dir):
+    os.mkdir(image_dir)
 
-    # If the directory doesn't exist, create it
-    if not os.path.isdir(image_dir):
-        os.mkdir(image_dir)
+# Initialize the image path (note the filetype should be png)
+image_path = os.path.join(image_dir, 'ch9-sol-generated-image.png')
 
-    # Initialize the image path (note the filetype should be png)
-    image_path = os.path.join(image_dir, 'ch9-sol-generated-image.png')
+# Retrieve the generated image
+# extract image URL from response
+image_url = generation_response["data"][0]["url"]
+generated_image = requests.get(image_url).content  # download the image
+with open(image_path, "wb") as image_file:
+    image_file.write(generated_image)
 
-    # Retrieve the generated image
-    image_url = generation_response["data"][0]["url"]  # extract image URL from response
-    generated_image = requests.get(image_url).content  # download the image
-    with open(image_path, "wb") as image_file:
-        image_file.write(generated_image)
+# Display the image in the default image viewer
+image = Image.open(image_path)
+image.show()
 
-    # Display the image in the default image viewer
-    image = Image.open(image_path)
-    image.show()
 
-# catch exceptions
-#except client.error.InvalidRequestError as err:
-#    print(err)
-
-finally:
-    print("completed!")
 # ---creating variation below---
-
-
 
 # response = openai.Image.create_variation(
 #   image=open(image_path, "rb"),
